b72f61581a84bfa926e049c47f363b5c5c486b23
Andrei <a.petrisor@studenti.unibg.it>
2025-09-04 09:41:05 +0200

    sum

--- 1-begin/hello.c
+++ 1-begin/hello.c
@@ -1,6 +1,12 @@
 #include <stdio.h> // preprocessore, appunto esegue prima queta riga, poi viene compilato il file
 
+int sum(int a, int b){
+    return a+b;
+}
+
+
 int main(void){
-    printf("hello world");
+    printf("hello world %d %d\n", sum(10,20));
+
     return 0;
 }
